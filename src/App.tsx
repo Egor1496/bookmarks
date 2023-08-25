b8e54e8b4ebf0aafd8193eb678acb2bd4960248f
Egor1496 <Egor1496Work@mail.ru>
2023-08-25 14:07:33 +0300

    0.0.1 gh pages, trs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Counter } from './features/counter/Counter';
 
+// Маршрутизация с использованием on gh-pages плохо работает с BrowserRouter or ReactRouter, вместо этого используйте HashRouter из react-router-dom.
+
 function App() {
   return (
     <>
--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -21,44 +21,30 @@ export function Counter() {
     <div>
       <div >
         <button
-
           aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
+          onClick={() => dispatch(decrement())}>
           -
         </button>
         <span >{count}</span>
         <button
-
           aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
+          onClick={() => dispatch(increment())}>
           +
         </button>
       </div>
       <div >
         <input
-
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
-        <button
-
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
+        <button onClick={() => dispatch(incrementByAmount(incrementValue))}>
           Add Amount
         </button>
-        <button
-
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
+        <button onClick={() => dispatch(incrementAsync(incrementValue))}>
           Add Async
         </button>
-        <button
-
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
+        <button onClick={() => dispatch(incrementIfOdd(incrementValue))}>
           Add If Odd
         </button>
       </div>
